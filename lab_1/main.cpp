76a9a2fc946502c62604fe68067a29fe0b9c6963
moskalt <thetima2@gmail.com>
2020-09-09 18:04:24 +0700

    Creating Tree

--- lab_1/main.cpp
+++ lab_1/main.cpp
@@ -29,18 +29,57 @@ public:
         return pVertex;
     }
 
-    void CreateTree(int size) {
+    void CreateTree(int size, Vertex *root) {
+        Vertex *root_ptr = root;
         int vertex_left_count = 0;
         int vertex_right_count = 0;
-        if ((size - 1) % 2 == 0) {
+        if ((size - 1) % 2 == 0 && size != 0) {
             vertex_right_count = vertex_left_count = (size - 1) / 2;
+            if (size == 1) {
+                root_ptr->ptr_l_Vertex = CreateVertex();
+                return;
+            }
             //creating new Vertexes
-        } else {
+            if (vertex_left_count != 0) {
+                root_ptr->ptr_l_Vertex = CreateVertex();
+                vertex_left_count--;
+            }
+            if (vertex_right_count != 0) {
+                root_ptr->ptr_r_Vertex = CreateVertex();
+                vertex_right_count--;
+            }
+            CreateTree(vertex_left_count, root_ptr->ptr_l_Vertex);
+            CreateTree(vertex_right_count, root_ptr->ptr_r_Vertex);
+
+
+        } else if ((size - 1) % 2 == 1 && size != 0) {
+            if (size == 1) {
+                root_ptr->ptr_l_Vertex = CreateVertex();
+                return;
+            }
             vertex_left_count = (size - 1) / 2 + 1;
             vertex_right_count = (size - 1) / 2;
             //creating new Vertexes
+            if (vertex_left_count != 0) {
+                root_ptr->ptr_l_Vertex = CreateVertex();
+                vertex_left_count--;
+            }
+            if (vertex_right_count != 0) {
+                root_ptr->ptr_r_Vertex = CreateVertex();
+                vertex_right_count--;
+            }
+            CreateTree(vertex_left_count, root_ptr->ptr_l_Vertex);
+            CreateTree(vertex_right_count, root_ptr->ptr_r_Vertex);
         }
+    }
 
+    void PrintLeftToRight(Vertex* pVertex){
+        if(pVertex != nullptr){
+            cout.width(4);
+            PrintLeftToRight(pVertex->ptr_l_Vertex);
+            cout << pVertex->ptr_l_Vertex->data << endl;
+            PrintLeftToRight(pVertex->ptr_r_Vertex);
+        }
     }
 
     Vertex *GetRoot() {
@@ -51,6 +90,7 @@ public:
 };
 
 int main() {
-
+    Tree tree;
+    tree.CreateTree(5,tree.GetRoot());
     return 0;
 }
