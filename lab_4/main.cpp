564e217a8bd69029493d616239fd5e91206c0d51
Morozov312 <alexander.morozov312@gmail.com>
2020-10-01 00:04:21 +0700

    delete function refactoring

--- lab_4/main.cpp
+++ lab_4/main.cpp
@@ -21,13 +21,11 @@ class Tree {
 private:
     vector<int> m_array;
     int m_size = 0;
-
     void fillVector(int tree_size) {
         for (int i = 0; i < tree_size; i++) {
             this->m_array.push_back(rand() % 400 - 100);
         }
     }
-
     void printVector() {
         cout << "Initial array" << endl;
         for (auto &i : m_array) {
@@ -35,7 +33,6 @@ private:
         }
         cout << endl;
     }
-
     static void addDoubleIndirection(int key, Vertex **root) {
         Vertex **head_ptr = root;
         while (*head_ptr) {
@@ -60,7 +57,6 @@ public:
         fillVector(m_size);
         printVector();
     }
-
     void printLeftToRight(Vertex *root) {
         if (root != nullptr) {
             printLeftToRight(root->ptrLeft);
@@ -68,7 +64,6 @@ public:
             printLeftToRight(root->ptrRight);
         }
     }
-
     void printTopToBottom(Vertex *root) {
         if (root != nullptr) {
             cout << root->data << " ";
@@ -76,7 +71,6 @@ public:
             printTopToBottom(root->ptrRight);
         }
     }
-
     void printBottomToTop(Vertex *root) {
         if (root != nullptr) {
             printBottomToTop(root->ptrLeft);
@@ -84,100 +78,43 @@ public:
             cout << root->data << " ";
         }
     }
-
-    int treeSize(Vertex *pVertex) {
-        if (pVertex == nullptr)
-            return 0;
-        else
-            return 1 + treeSize(pVertex->ptrLeft) + treeSize(pVertex->ptrRight);
-    }
-
-    int treeControlSum(Vertex *root) {
-        if (root == nullptr)
-            return 0;
-        else
-            return root->data + treeControlSum(root->ptrLeft) +
-                   treeControlSum(root->ptrRight);
-    }
-
-    int maxTreeHeight(Vertex *root) {
-        if (root == nullptr) {
-            return 0;
-        } else {
-            return 1 +
-                   max(maxTreeHeight(root->ptrLeft), maxTreeHeight(root->ptrRight));
-        }
-    }
-
-    float averageTreeHeight(Vertex *root, int level = 1) {
-        if (root == nullptr) {
-            return 0;
-        } else {
-            return (float) level + averageTreeHeight(root->ptrLeft, level + 1) +
-                   averageTreeHeight(root->ptrRight, level + 1);
-        }
-    }
-
-    int searchInTree(Vertex *root, int key) {
-        if (key == root->data) {
-            cout << root->data;
-            return 1;
-        }
-        if (root->ptrRight == nullptr && root->ptrLeft == nullptr) {
-            return 0;
-        }
-        if (key > root->data) {
-            cout << root->data << " ";
-            searchInTree(root->ptrRight, key);
-        } else if (key < root->data) {
-            cout << root->data << " ";
-            searchInTree(root->ptrLeft, key);
-        }
-        return 0;
-    }
-
     void buildDoubleIndirection(Vertex **pVertex) {
         for (auto &item : this->m_array) {
             addDoubleIndirection(item, pVertex);
         }
     }
-
-    static void DeleteElement(int key, Vertex *root_ptr) {
-        Vertex **root = &root_ptr;
-        Vertex *r, *s, *q;
-        r = CreateVertex();
-        s = CreateVertex();
-        q = CreateVertex();
-        while (*root != nullptr) {
-            if ((*root)->data < key) {
-                root = &(*root)->ptrRight;
-            } else if ((*root)->data > key) {
-                root = &(*root)->ptrLeft;
+    static void DeleteElement(int key, Vertex *root) {
+        Vertex **ptrRoot = &root;
+        Vertex *temp1, *temp2, *temp3;
+        while (*ptrRoot != nullptr) {
+            if ((*ptrRoot)->data < key) {
+                ptrRoot = &(*ptrRoot)->ptrRight;
+            } else if ((*ptrRoot)->data > key) {
+                ptrRoot = &(*ptrRoot)->ptrLeft;
             } else {
-                cout << "error " << endl;
+                cout << "Delete element:" << (*ptrRoot)->data << endl;
                 break;
             }
         }
-        if (*root != nullptr) {
-            q = *root;
-            if (q->ptrLeft == nullptr) {
-                *root = q->ptrRight;
-            } else if (q->ptrRight == nullptr) {
-                *root = q->ptrLeft;
+        if (*ptrRoot != nullptr) {
+            temp3 = *ptrRoot;
+            if (temp3->ptrLeft == nullptr) {
+                *ptrRoot = temp3->ptrRight;
+            } else if (temp3->ptrRight == nullptr) {
+                *ptrRoot = temp3->ptrLeft;
             } else {
-                r = q->ptrLeft;
-                s = q;
-
-                while (r->ptrRight != nullptr) {
-                    s = r;
-                    r = r->ptrRight;
+                temp1 = temp3->ptrLeft;
+                temp2 = temp3;
+                while (temp1->ptrRight != nullptr) {
+                    temp2 = temp1;
+                    temp1 = temp1->ptrRight;
                 }
-                s->ptrRight = r->ptrLeft;
-                r->ptrLeft = q->ptrRight;
-                r->ptrRight = q->ptrRight;
-                *root = r;
+                temp2->ptrRight = temp1->ptrLeft;
+                temp1->ptrLeft = temp3->ptrRight;
+                temp1->ptrRight = temp3->ptrRight;
+                *ptrRoot = temp1;
             }
-            delete q;
+            delete temp3;
         }
     }
 };
@@ -190,8 +127,11 @@ int main() {
     tree1.buildDoubleIndirection(&head1);
     cout << "LeftToRight:" << endl;
     tree1.printLeftToRight(head1);
-    Tree::DeleteElement(178, head1);
-    cout << endl << "LeftToRight:" << endl;
+    int delElement = -42;
+    cout << endl
+         << "Try to delete element: " << delElement << endl;
+    Tree::DeleteElement(delElement, head1);
+    cout << "LeftToRight:" << endl;
     tree1.printLeftToRight(head1);
     return 0;
 }
