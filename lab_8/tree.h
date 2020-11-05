72a0dc10dd0c0952fb82496cfb26989ee862d890
moskalt <thetima2@gmail.com>
2020-11-05 13:03:07 +0700

    AW matrix

--- lab_8/tree.h
+++ lab_8/tree.h
@@ -133,8 +133,10 @@ public:
                 AWmatrix[i][j] = 0;
             }
         }
-        for (size_t i = 1; i < AWmatrix.size(); i++) {
-            AWmatrix[i - 1][i] = weights_array[i - 1];
+        for (size_t i = 0; i < AWmatrix.size(); i++) {
+            for (size_t j = i +1; j < AWmatrix.size(); j++) {
+                AWmatrix[i][j] = AWmatrix[i][j -1 ] + weights_array[j - 1];
+            }
         }
     }
 
@@ -147,4 +149,5 @@ public:
             }
         }
     }
+
 };
